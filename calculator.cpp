3d3d65a69d40e4e56b5327349a59b774dae5f46c
DimitrisPa <pappasdimitris98@gmail.com>
2023-02-24 13:57:46 +0200

    Update code and README

--- calculator.cpp
+++ calculator.cpp
@@ -3,63 +3,82 @@ using namespace std;
 
 int main()
 {
-    //Welcome Screen
-    cout<<"Welcome to the CLI Calculator\n\n";
+    // Welcome Screen
+
+    cout << "Welcome to the CLI Calculator\n\n";
     string choice;
 
-    do{
-
-    //User choice
-    cout<<"Select value \n1) +\n2) -\n3) *\n4) /\n5) x (Exit)\n\n";
-    char input;
-    cout<<"Value : ";
-    cin>>input;
-    cout<<"\n";
-
-    //Terminate the app
-    if (input == 'x')
+    do
     {
-        exit (EXIT_FAILURE);;
-    }else
-    {
-    //User numbers
-    double x1,x2;
-    cout<<"Please insert two numbers : ";
-    cin >> x1 >> x2;
-    cout<<"\n";
 
-    switch(input)
-    {
-        //Addition
-        case '+' :
-            cout<<"Result is : "<< x1 <<" + "<< x2 <<" = "<< x1 + x2<<"\n\n";
-        break;
-        //Subtraction
-        case '-' :
-            cout<<"Result is : "<< x1 <<" - "<< x2 <<" = "<< x1 - x2<<"\n\n";
-        break;
-        //Multiplication
-        case '*' :
-            cout<<"Result is : "<< x1 <<" * "<< x2 <<" = "<< x1 * x2<<"\n\n";
-        break;
-        //Division
-        case '/' :
-            cout<<"Result is : "<< x1 <<" / "<< x2 <<" = "<< x1 / x2<<"\n\n";
-        break;
-        //Wrong input
-        default :
-            cout<<"Wrong value\n"<<"\n";
-        break;
+        // User selection
 
-    }
-    }
-    //Give user the choice to restart the app
-    cout<<"Would you like to use the calculator again? [Yes/No] : ";
-    cin>>choice;
-    cout<<"\n";
+        cout << "Select value \n1) +\n2) -\n3) *\n4) /\n5) x (Exit)\n\n";
+        char input;
+        cout << "Value : ";
+        cin >> input;
+        cout << "\n";
 
-    }
-    while(choice == "Yes" || choice == "yes" || choice == "y");
+        // Exit the application
+
+        if (input == 'x')
+        {
+            exit(EXIT_FAILURE);
+            ;
+        }
+        else
+        {
+
+            // User entry
+
+            double x1, x2;
+            cout << "Please enter two numbers : ";
+            cin >> x1 >> x2;
+            cout << "\n";
+
+            switch (input)
+            {
+
+                // Addition
+
+            case '+':
+                cout << "The result is : " << x1 << " + " << x2 << " = " << x1 + x2 << "\n\n";
+                break;
+
+                // Subtraction
+
+            case '-':
+                cout << "The result is : " << x1 << " - " << x2 << " = " << x1 - x2 << "\n\n";
+                break;
+
+                // Multiplication
+
+            case '*':
+                cout << "The result is : " << x1 << " * " << x2 << " = " << x1 * x2 << "\n\n";
+                break;
+
+                // Division
+
+            case '/':
+                cout << "The result is : " << x1 << " / " << x2 << " = " << x1 / x2 << "\n\n";
+                break;
+
+                // Wrong input
+
+            default:
+                cout << "Wrong value\n"
+                     << "\n";
+                break;
+            }
+        }
+
+        // Allow the user to choose to restart the application
+
+        cout << "Do you want to use the calculator again? [Yes/No] : ";
+        cin >> choice;
+        cout << "\n";
+
+    } while (choice == "Yes" || choice == "yes" || choice == "y");
 
     return 0;
 }
